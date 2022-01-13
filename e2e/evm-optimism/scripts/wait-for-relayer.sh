44ad4ce6d7e0918d6e9beee6d5eb1d2a526255d4
Matija Petrunic <matija.petrunic@fer.hr>
2022-01-13 14:53:06 +0100

    Increase wait for relayer retries

--- e2e/evm-optimism/scripts/wait-for-relayer.sh
+++ e2e/evm-optimism/scripts/wait-for-relayer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-RETRIES=30
+RETRIES=150
 i=0
 until docker-compose -f docker-compose-nobuild.yml -f docker-compose.e2e.yml logs relayer1 | grep -q "Starting relayer";
 do
-    sleep 3
+    sleep 5
     if [ $i -eq $RETRIES ]; then
         echo 'Timed out waiting for relayer'
         break
